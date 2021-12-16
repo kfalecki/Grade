c5cdfadc87483d540db71090831da84be37dce19
Kacper Falęcki <93715688+kfalecki@users.noreply.github.com>
2021-12-16 23:10:09 +0100

    Add files via upload

---
+++ Grade.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Grade
+{
+    public Subjects subject;
+    public int grade;
+
+    public Grade(Subjects subject, int grade)
+    {
+        this.subject = subject;
+        this.grade = grade;
+    }
+}
